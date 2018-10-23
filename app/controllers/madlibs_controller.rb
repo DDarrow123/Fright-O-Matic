7433c6d9262e0f5827b8c841db7a26e163e46b52
DDarrow123 <ddarrowphl@gmail.com>
2018-10-23 12:24:27 -0400

    built new form out finally

--- app/controllers/madlibs_controller.rb
+++ app/controllers/madlibs_controller.rb
@@ -1,14 +1,20 @@
 class MadlibsController < ApplicationController
 
   def new_witch
-    initiate
+    @witch = Madlib.new
     @nouns = Word.where(category_id: 1, w_type: 'noun')
     @verbs = Word.where(category_id: 1, w_type: 'verb')
     @adjectives = Word.where(category_id: 1, w_type: 'adjective')
+    @c1 = Category.find(1)
   end
 
   def create_witch
-    #code
+    @witch = Madlib.create(witch_params)
+    redirect_to witches_show_path(@witch)
+  end
+
+  def show_witch
+    @witch = Madlib.find(params[:id])
   end
 
   def new_ghost
@@ -21,6 +27,12 @@ class MadlibsController < ApplicationController
 
   def new_ghost
     initiate
+  end
+
+  private
+
+  def witch_params
+    params.require(:madlib).permit(:category_id, :word1_id, :word2_id, :word3_id, :word4_id, :word5_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :madlibs
+  # resources :madlibs
   # resources :categories
   # resources :words
   # resources :pranks
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
    root 'pranks#home', as: 'home'
    get '/index' => 'categories#index', as: 'category_index'
    get '/witches_new'  => 'madlibs#new_witch', as: 'witches_new'
-  # post '/madlib_new' => 'madlibs#create_witch', as: 'witches_create'
+  post '/witches_create' => 'madlibs#create_witch', as: 'witches_create'
+  get '/witches_show/:id' => 'madlibs#show_witch', as: 'witches_show'
   #don't need above path because it's available through the madlibs resource
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
