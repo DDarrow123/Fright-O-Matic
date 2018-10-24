b3c44b2ef1aa7d02029a9a6817946954cd354abd
DDarrow123 <ddarrowphl@gmail.com>
2018-10-24 17:37:18 -0400

    built all features and started styling welcome page

---
+++ app/assets/javascripts/madlibs.js
@@ -0,0 +1,24 @@
+// var counter = 0;
+// var timeleft = 60;
+//
+// function setup() {
+//  noCanvas();
+//
+//  var timer = select('#timer')
+//  timer.html(timeleft - counter)
+//
+//  function timeIt() {
+//    counter ++;
+//    timer.html(timeleft - counter);
+//
+//  }
+//
+//  setInterval(timeIt, 1000);
+//
+// }
+// // setup()
+// function sayHi(){
+//   debugger;
+// }
+// console.log("I've been loaded!");
+// sayHi()
